da4fd40e76405613c270a71630dae72563ef8684
MaxChandia <maximiliano.chandiaf@gmail.com>
2024-05-10 02:53:07 -0400

    prueba gif

--- src/components/carrousel.jsx
+++ src/components/carrousel.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import sec from '../sec.gif';
+import sec2 from '../sec2.gif';
 import '../styles/carrousel.css'
 import { Link } from "react-router-dom";
 
 const Carrousel = () => {
     return(
         <div className="slideContainer">
-            <img src={sec} className="" />
+            <img src={sec2} className="" />
             <div className="slideText">Más de 10 años aportando al rubro de la metalmecánica</div>
             <Link to="/contacto">
             <button className="slideButton" style={{textDecoration:"none"}}>
